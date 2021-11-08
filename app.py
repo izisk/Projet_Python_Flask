ea8ef6547f80d4f75511b033164a2728c5125f8e
Izabell <isk.izabell@gmail.com>
2021-11-08 23:18:20 +0100

    version json

--- app.py
+++ app.py
@@ -7,104 +7,99 @@ from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import csv
 import json
-
+import os
 
 # app
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite:///sqlitedb.file"
+file_path = os.path.abspath(os.getcwd())+"\database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///'+file_path
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
 
+###
+#Filling the JSON file with Date Price Open High Low 
+columns = ['Date', 'Price', 'Open', 'High', 'Low', "Vol.", "Change %"]
+file_csv = open('BTC_USD-Bitfinex-Historical-Data.csv', 'r')
+reader = csv.DictReader(file_csv, columns)
+file_json = open('affichage.json', 'w')
+L = list(reader)
+for i in range(1,len(L)) :
+    del L[i]["Vol."]
+    del L[i]["Change %"]
+    json.dump(L[i], file_json, indent=0)
 
+file_csv.close()
+file_json.close()
 
-columns = ['Price', 'Open', 'High', 'Low']
-data_base_csv = pd.read_csv('BTC_USD-Bitfinex-Historical-Data.csv', names=columns)
-data_base_html = data_base_csv.to_html("affichage.html")
+#Affiche tout
+#json.dumps(list(csv.reader(open('BTC_USD-Bitfinex-Historical-Data.csv'))))
 
-db = SQLAlchemy(app)
-now = datetime.now()
-
-# Model
-"""
-class data(db.Model) :
-    __tablename__ = "data"
-    Date = db.column(db.Date)
-    Price = db.column(db.Float)
-    Open = db.column(db.Float)
-    High = db.column(db.Float)
-    Low = db.column(db.Float)
-"""
-
-@app.route("/")
-def home():
-    return render_template("affichage.html")
-
-
-if __name__ == "__main__":
-    #db.create_all()
-    app.run()
-    
-
-"""
-
-
-# configure sqlite3 to enforce foreign key contraints
-'''
 @event.listens_for(Engine, "connect")
 def _set_sqlite_pragma(dbapi_connection, connection_record):
     if isinstance(dbapi_connection, SQLite3Connection):
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON;")
         cursor.close()
-'''
-    
-'''
-class User(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    address = db.Column(db.String(200))
-    phone = db.Column(db.String(50))
-    posts = db.relationship("BlogPost", cascade="all, delete")
 
 
-class BlogPost(db.Model):
-    __tablename__ = "blog_post"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    body = db.Column(db.String(200))
-    date = db.Column(db.Date)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-'''
-    
-# routes
-'''
-@app.route("/user", methods=["POST"])
-def create_user():
-    try:
-        data = request.get_json()
-        if data is None:
-            return jsonify({"error": "missing data, please enter it in the body!" }), 400
+db = SQLAlchemy(app)
+now = datetime.now()
 
-        for field in ["name", "email", "address", "phone"]:
-            if field not in data.keys():
-                return jsonify({"error": "no " + field + " in data" }), 400
+# Model
+class bitcoin(db.Model) :
+    __tablename__ = "bitcoin"
+    Date = db.Column(db.Date, primary_key=True)
+    Price = db.Column(db.Float)
+    Open = db.Column(db.Float)
+    High = db.Column(db.Float)
+    Low = db.Column(db.Float)
+
+
+@app.route("/")
+def home():
+    return "<h1>API Running</h1>"
+
         
-        new_user = User(
-            name=data["name"],
-            email=data["email"],
-            address=data["address"],
-            phone=data["phone"],
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({"message": "User created"}), 200
+# routes
+@app.route("/bitcoin", methods=['GET', 'POST'])
+def create_bitcoin():
+    try :
+        if request.method == "POST" :
+            data = request.get_json()
+            if data is None:
+                return jsonify({"error": "missing data, please enter it in the body!" }), 400
+
+            for field in ["Date", "Price", "Open", "High", "Low"]:
+                if field not in data.keys():
+                    return jsonify({"error": "no " + field + " in data" }), 400
+            
+            new_data = bitcoin(
+                Date = data["Date"],
+                Price = data["Price"],
+                Open = data["Open"],
+                High = data["High"],
+                Low = data["Low"],
+            )
+            db.session.add(new_data)
+            db.session.commit()
+            return jsonify({"message": "User created"}), 200
+        else :
+            return render_template('affichage.json')
     except Exception as e:
-        return jsonify({"error": "Failed, caught exception" + str(e)}), 400
+        return jsonify({"error": "Failed, caught exception " + str(e)}), 400
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run()
+    with app.test_request_context('/bitcoin', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+        create_bitcoin()
+        assert request.path == '/bitcoin'
+        assert request.method == 'POST'
 
 
+'''
 @app.route("/user/descending_id", methods=["GET"])
 def get_all_users_descending():
     users = User.query.all()
@@ -142,5 +137,3 @@ def get_all_users_ascending():
 
     return jsonify(all_users_ll.to_list()), 200
 '''
-
-"""
